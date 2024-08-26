82294f5421448f720a14cce60d08aec118ed20a3
Max <maxmadera04@gmail.com>
2024-08-26 01:31:10 -0400

    progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './pages/home/Home'
 function App() {
 
 
   return (
    <div className='p-4 h-screen flex items-center justify-center'>
-    <Login />
+    <Home/>
    </div>
   )
 }
