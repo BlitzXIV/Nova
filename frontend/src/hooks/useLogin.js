3bdfb8a90d78e4de8732400142f7e3b361bc540a
Max <maxmadera04@gmail.com>
2024-09-02 16:13:09 -0400

    get messages complete

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -6,12 +6,15 @@ const useLogin = () => {
 const [loading, setLoading] = useState(false)
 const {setAuthUser} = userAuthContext()
     const login = async (username, password) => {
+        const success = handleInputErrors(username,password)
+        if(!success) return
+
         setLoading(true)
         try {
             const res = await fetch("api/auth/login", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(username, password)
+                body: JSON.stringify({username, password})
             })
             const data = await res.json()
             if (data.error) {
@@ -35,3 +38,12 @@ const {setAuthUser} = userAuthContext()
 
 
 export default useLogin
+
+
+function handleInputErrors(username,password){
+    if(!username || !password ){
+        toast.error("Please Fill all Fields")
+        return false
+    }
+    return true
+}
