c1963d3ea45f981a045e9b2f438759258f0d1de6
Max <maxmadera04@gmail.com>
2024-09-01 20:40:57 -0400

    .

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Login
-					<span className='text-blue-500'> ChatApp</span>
+					Login To
+					<span className='text-purple-500'> Nova</span>
 				</h1>
 
 				<form>
@@ -25,9 +27,10 @@ const Login = () => {
 							className='w-full input input-bordered h-10'
 						/>
 					</div>
-					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
+					
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2'>Login</button>
