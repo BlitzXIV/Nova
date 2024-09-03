cd4e7f6078d1d5e5d0fdcb05abe6919a73351734
Max <maxmadera04@gmail.com>
2024-09-03 02:10:48 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
+import path from "path"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -10,7 +11,7 @@ import userRoutes from "./routes/user.routes.js"
 import mongoConnect from "./db/mongoConnect.js"
 import { app, server } from "./socket/socket.js"
 
-
+const __dirname = path.resolve()
 const PORT = process.env.PORT || 5000
 
 dotenv.config()
@@ -23,8 +24,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "./frontend/dist")))
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend/dist/index.html"))
+})
 
 server.listen(PORT,() =>{  
     mongoConnect()
